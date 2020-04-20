06d61794becd20d8b77a320fd134f1db4cffc010
Nikolay Romeiko <nickolay.romeyko@gmail.com>
2020-04-20 12:39:45 +0300

    conftest.py fix, updated system/test_user.py - registration, login-auth, register user with the same name

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,16 +3,22 @@ from app import app
 from db import db
 
 
-@pytest.fixture(scope="function")
-def tc_app_db_env():
+@pytest.fixture(scope="session")
+def init_app():
     # Make sure Database exists
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'
     with app.app_context():
         db.init_app(app)
+    yield app
+
+
+@pytest.yield_fixture(scope="function")
+def tc_app_db_env(init_app):
+    with init_app.app_context():
         db.create_all()
-    # Getting a test client
-    yield app.test_client, app.app_context
+    # Testing begin here
+        yield init_app.test_client, init_app.app_context
     # Cleaning Database after test
-    with app.app_context():
+    with init_app.app_context():
         db.session.remove()
         db.drop_all()
